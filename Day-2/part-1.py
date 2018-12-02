716c27e2e36068ee7e448ba83bc4f002e71ff337
Daniel Coffaro <danielbcoffaro@gmail.com>
2018-12-02 08:31:47 +0100

    part 2 day 2 complete

---
+++ Day-2/part-1.py
@@ -0,0 +1,19 @@
+
+from collections import Counter
+
+filename = "data"
+file = open(filename, "r")
+
+counter2 = 0
+counter3 = 0
+exists2 = False
+exists3 = False
+
+for line in file:
+
+    counts = Counter(line.strip())
+    if 2 in counts.values():
+        counter2 += 1
+    if 3 in counts.values():
+        counter3 += 1
+print(counter2 * counter3)
