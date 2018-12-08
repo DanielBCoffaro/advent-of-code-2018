795a27572979d681c785b65d72695b8bbf862984
Daniel Coffaro <danielbcoffaro@gmail.com>
2018-12-08 20:34:42 +0100

    day 3 updates

--- Day-4/part-1.py
+++ Day-4/part-1.py
@@ -8,9 +8,9 @@ lines.sort()
 
 guards = {}
 
-for i, x in enumerate(lines):
+for x in lines:
     if 'Guard' in x:
-        currentguard = guard_line = parse('[1518-{date} {time}] Guard #{id} begins shift', x)
+        currentguard = parse('[1518-{date} {time}] Guard #{id} begins shift', x)
     if 'asleep' in x:
         sleep_time = parse('[1518-{date} 00:{time}] falls asleep', x)
     if 'wakes' in x:
@@ -19,9 +19,10 @@ for i, x in enumerate(lines):
         if currentguard['id'] not in guards:
             guards[currentguard['id']] = 0
         guards[currentguard['id']] += timeasleep
-sleepiestguard = max(guards, key=guards.get)
 
+sleepiestguard = max(guards, key=guards.get)
 minutesasleep = []
+
 for i, x in enumerate(lines):
     if 'Guard' in x:
         currentguard = guard_line = parse('[1518-{date} {time}] Guard #{id} begins shift', x)
@@ -33,7 +34,6 @@ for i, x in enumerate(lines):
         if currentguard['id'] not in guards:
             guards[currentguard['id']] = 0
         minutesasleep = minutesasleep + list(range(int(sleep_time['time']), int(wake_time['time'])))
-
 
 minutecount = Counter(minutesasleep).most_common(1)
 print(int(sleepiestguard) *  minutecount[0][0])
